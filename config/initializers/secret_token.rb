7975b2da74e9bb2c3bba918e7eb6f2909253360b
Allan Ripley <allan@arbiontec.com>
2010-11-25 01:33:47 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = '6e7e4b26cd43f96347ca1572d057c8f5353bf72d50f936553372e2f7c7230a7e206e2d1d0ebc4df0f3b89e8887655e52ff4e82420ba9d64788dbc8aa0327d0c0'
